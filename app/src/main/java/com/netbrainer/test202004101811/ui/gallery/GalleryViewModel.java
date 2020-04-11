fe8ba6cd6d84b90f761eaf57b1e86317107dcbfc
mkamoski <mkamoski@users.noreply.github.com>
2020-04-11 11:09:51 -0400

    Initial commit

---
+++ app/src/main/java/com/netbrainer/test202004101811/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,19 @@
+package com.netbrainer.test202004101811.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is gallery fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
